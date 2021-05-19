748958af9e54c89f6a71904a5463f1e4f3de4c42
Ernesto Pelaez <epmjj4@hotmail.com>
2021-05-19 19:55:09 -0400

    created Nav bar

---
+++ src/components/Nav/MyNavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './MyNavBar.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+
+
+
+function MyNavBar() {
+    return (
+        <div>
+            <Navbar fixed="top" bg="light" expand="lg">
+                <Navbar.Brand href="#home">Ernesto Pelaez</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default MyNavBar
