305385ddb34feb1b5c5dec31954bcf6bca490330
Ernesto Pelaez <epmjj4@hotmail.com>
2021-05-19 22:51:01 -0400

    added images and screenshot

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { ThemeProvider } from 'styled-components';
+import { theme } from './themes/theme';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <ThemeProvider theme={theme}>
+  <App />
+  </ThemeProvider>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  document.getElementById('root')
+  );
+  
+  
+  
+
+
 
